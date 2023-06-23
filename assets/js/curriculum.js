21e67c1e982d21252d9195993f6ef8f56cbf4021
valeiras <d.r.valeiras@gmail.com>
2023-06-23 05:49:35 +0200

    Better responsiveness on mobile

--- assets/js/curriculum.js
+++ assets/js/curriculum.js
@@ -20,7 +20,9 @@ button.addEventListener('click', generatePDF);
 function scaleCV() {
    const cvContainer = document.getElementById('cv-container');
    const cvWidth = cvContainer.getBoundingClientRect().width;
-   const windowWidth = window.innerWidth;
+   // const windowWidth = window.innerWidth;
+   // const windowWidth = screen.width;
+   const windowWidth = document.documentElement.clientWidth;
    if (cvWidth > windowWidth) {
       const r = windowWidth / cvWidth;
       cvContainer.style.transform = `scale(${r})`;
