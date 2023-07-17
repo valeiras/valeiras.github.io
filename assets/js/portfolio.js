4333a8f353fd406f4e9d95d2e7aaf45119e18a5e
valeiras <d.r.valeiras@gmail.com>
2023-07-17 16:48:34 +0200

    Adds "Read more" buttons

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,33 +1,48 @@
 function exitFullpage() {
-   //$('#img-gallery').removeClass('active');
-   $("#img-gallery").removeAttr("style");
-   $('#img-gallery').toggleClass(['active', 'inactive']);
-   $("body").css("overflow-y", "visible");
+  //$('#img-gallery').removeClass('active');
+  $('#img-gallery').removeAttr('style');
+  $('#img-gallery').toggleClass(['active', 'inactive']);
+  $('body').css('overflow-y', 'visible');
 }
 
 function enterFullPage() {
-   $('#img-gallery').toggleClass(['active', 'inactive']);
-   $("#img-gallery").css({
-      "backgroundImage": "url(" + this.src + ")",
-      "top": window.scrollY + "px",
-      "height": window.outerHeight + "px"
-   })
-   $("body").css("overflow-y", "hidden");
+  $('#img-gallery').toggleClass(['active', 'inactive']);
+  $('#img-gallery').css({
+    backgroundImage: 'url(' + this.src + ')',
+    top: window.scrollY + 'px',
+    height: window.outerHeight + 'px',
+  });
+  $('body').css('overflow-y', 'hidden');
 }
 
-$("img:not(#header-img)").on("click", enterFullPage);
-$("#img-gallery").on("click", exitFullpage);
-$(document).on("keydown", exitFullpage);
+$('img:not(#header-img)').on('click', enterFullPage);
+$('#img-gallery').on('click', exitFullpage);
+$(document).on('keydown', exitFullpage);
 
 // Adjust the top margin of the first section, so there is no content overlap:
 const firstSection = $('section:first-of-type');
 const navBar = $('#nav-bar');
 
-function positionFirstSection() {
-   firstSection.css('padding-top', (parseFloat(navBar.css('height')) + 75) + 'px');
-};
+function addBtnListener(selector) {
+  $(`#btn-${selector}-more`).on('click', () => {
+    $(`#btn-${selector}-more`).css({ visibility: 'hidden' });
+    $(`#${selector}-2`).css({ display: 'block' });
+  });
 
-// We adjust it when the DOM is loaded, and on window resize
-$(document).on("DOMContentLoaded", positionFirstSection);
-$(window).on('resize', positionFirstSection);
+  $(`#btn-${selector}-less`).on('click', () => {
+    $(`#btn-${selector}-more`).css({ visibility: 'visible' });
+    $(`#${selector}-2`).css({ display: 'none' });
+  });
+}
 
+const sections = [
+  'mekanika',
+  'pequena-banda-robotica',
+  'household-music',
+  'phd',
+  'music',
+  'dald',
+];
+sections.forEach((selector) => {
+  addBtnListener(selector);
+});
