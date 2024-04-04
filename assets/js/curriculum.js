62daf2c6ac6ab0940210a8b38b9edf968a62d020
David Reverter Valeiras <d.r.valeiras@gmail.com>
2024-04-04 09:15:16 +0200

    Update the CV information

--- assets/js/curriculum.js
+++ assets/js/curriculum.js
@@ -1,31 +1,31 @@
-const button = document.getElementById('generate-btn');
+const button = document.getElementById("generate-btn");
 
 let options = {
-   // margin: -10,
-   filename: 'CV_David_Reverter_Valeiras',
-   jspdf: { unit: 'mm', format: 'a4' },
-   html2canvas: { dpi: 300, scale: 3 }
-}
+  // margin: -10,
+  filename: "CV_David_Reverter_Valeiras",
+  jspdf: { unit: "mm", format: "a4" },
+  html2canvas: { dpi: 300, scale: 3 },
+};
 
 function generatePDF() {
-   const element = document.getElementById('cv-container');
-   options.html2canvas.scrollX = -8;
-   // element.getBoundingClientRect.left;
-   // Choose the element and save the PDF for your user.
-   html2pdf().set(options).from(element).save();
+  const element = document.getElementById("cv-container");
+  options.html2canvas.scrollX = -8;
+  // element.getBoundingClientRect.left;
+  // Choose the element and save the PDF for your user.
+  html2pdf().set(options).from(element).save();
 }
 
-button.addEventListener('click', generatePDF);
+button.addEventListener("click", generatePDF);
 
 function scaleCV() {
-   const cvContainer = document.getElementById('cv-container');
-   const cvWidth = cvContainer.getBoundingClientRect().width;
-   const windowWidth = document.documentElement.clientWidth;
-   if (cvWidth > windowWidth) {
-      const r = windowWidth / cvWidth;
-      document.body.style.transform = `scale(${r})`;
-      document.body.style.transformOrigin = "0 0";
-   }
+  const cvContainer = document.getElementById("cv-container");
+  const cvWidth = cvContainer.getBoundingClientRect().width;
+  const windowWidth = document.documentElement.clientWidth;
+  if (cvWidth > windowWidth) {
+    const r = windowWidth / cvWidth;
+    document.body.style.transform = `scale(${r})`;
+    document.body.style.transformOrigin = "0 0";
+  }
 }
 
 document.addEventListener("DOMContentLoaded", scaleCV);
