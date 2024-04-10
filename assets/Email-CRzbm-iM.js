e7b5deade65947c3ab753034694419a81a5ecf61
David Reverter Valeiras <d.r.valeiras@gmail.com>
2024-04-10 10:41:04 +0200

    Updates

--- assets/Email-CRzbm-iM.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-1cLWtNaT.js";const i=()=>a.jsx("a",{href:"mailto:d.r.valeiras@gmail.com",children:"d.r.valeiras@gmail.com"});export{i as default};
---
+++ assets/Email-DYzTRyt1.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-BMZd9hJx.js";const i=()=>a.jsx("a",{href:"mailto:d.r.valeiras@gmail.com",children:"d.r.valeiras@gmail.com"});export{i as default};
